5121f84b5cd176117b9bdc41998cc55f6190d8b5
Mohd Shaquib Siddiquie <mdshaquibsiddiquie@gmail.com>
2025-05-31 16:40:26 +0530

    Transit

--- src/Component/Enter/Enter1.jsx
+++ src/Component/Enter/Enter1.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import contentImg from "../../Images/CONTENT.png";
+import { useNavigate } from "react-router-dom";
 
-const Enter1 = () => {
+const Enter1 = () => { 
+    const navigate = useNavigate()
   return (
-    <div className="bg-[#40189d]  h-screen flex justify-center items-center">
-      <img src={contentImg} alt="Content" />
+    <div  className="bg-[#40189d]  h-screen flex justify-center items-center">
+      <img onClick={()=> navigate("/Enter2")} src={contentImg} alt="Content" />
     </div>
   );
 };
---
+++ src/Component/Enter/Enter2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Enter2 = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Enter2
