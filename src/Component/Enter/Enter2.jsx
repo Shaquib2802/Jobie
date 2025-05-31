fbabca64041ecbbe5d0d9e4d7403c3145d645fb3
Mohd Shaquib Siddiquie <mdshaquibsiddiquie@gmail.com>
2025-05-31 17:03:14 +0530

    I a here

--- src/Component/Enter/Enter2.jsx
+++ src/Component/Enter/Enter2.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
+import React from "react";
 import bagImg from "../../Images/business 1.svg";
-
 
 const Enter2 = () => {
   return (
-    <div className='  h-screen flex justify-center items-center'>
-        <div className='flex flex-col border '>
-  <img src={bagImg} alt="" />
-        </div>
-    
+    <div className="  h-screen flex justify-center items-center">
+      <div className="flex flex-col border ">
+        <img src={bagImg} alt="" />
+      </div>
+      <div>Find your dream job now</div>
     </div>
-  )
-}
+  );
+};
 
-export default Enter2
+export default Enter2;
